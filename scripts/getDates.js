115684c339ac9dfbeda5f32c1859df04bcb8d98f
Miranda Saenz <sae22006@byui.edu>
2023-06-10 09:48:00 -0700

    Completed Week 01 Assignment

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+
+const options = {year: 'numeric'}
+
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+document.getElementById('lastModified').textContent = document.lastModified;

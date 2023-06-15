837cbb1d22beceecdbd8bbae730e53a7b4f718be
Miranda Saenz <sae22006@byui.edu>
2023-06-15 09:13:04 -0700

    Updates for Team Activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,26 @@ const options = {year: 'numeric'}
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
 document.getElementById('lastModified').textContent = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#073b4cff";
+		main.style.color = "#000";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});

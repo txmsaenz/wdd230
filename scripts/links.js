e6542c80bdd888d0d8c0fc88dfa42caaaa16ffab
Miranda Saenz <sae22006@byui.edu>
2023-07-12 13:30:14 -0700

    Updated Week 5

--- scripts/links.js
+++ scripts/links.js
@@ -5,7 +5,27 @@ const linksURL = "https://txmsaenz.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
+    displayLinks(data);
   }
   
   getLinks();
+
+  const displayLinks = (data) => {
+    const list = document.getElementById('linksList');
+
+    data.forEach((week) => {
+        let listItem = document.createElement('li');
+        let title = document.createElement('p');
+        let url = document.createElement('a');
+
+        title.innerText = week.title;
+        url.innerText = week.url;
+        url.href = linksURL + week.url;
+
+        listItem.appendChild(title);
+        listItem.appendChild(url);
+
+        list.appendChild(listItem);
+    });
+  }

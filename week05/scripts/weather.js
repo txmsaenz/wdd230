f7ca955a31d1ce60a36ab2c8af5f332b4aee8d0b
Miranda Saenz <sae22006@byui.edu>
2023-07-11 18:30:39 -0700

    Updated weather

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -10,8 +10,8 @@ async function apiFetch() {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data);
-      //displayResults(data); 
+      //console.log(data);
+      displayResults(data); 
     } else {
         throw Error(await response.text());
     }
@@ -22,3 +22,11 @@ async function apiFetch() {
 
 apiFetch();
 
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}

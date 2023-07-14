6fd0c00ccb13bbd4df8d35936e5b4c72e6e98b8c
Miranda Saenz <sae22006@byui.edu>
2023-07-13 18:46:37 -0700

    More Updates

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -28,3 +28,25 @@ lastVisit++;
 
 localStorage.setItem("lastVisit-ls", lastVisit);
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#073b4cff";
+		main.style.color = "#000";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});

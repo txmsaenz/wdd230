c7081273b1b2e2d26b44eec0fa0d2ce3aa6a36bf
Miranda Saenz <sae22006@byui.edu>
2023-07-15 18:16:24 -0700

    Hopefully final update

---
+++ chamber/scripts/lastVisited.js
@@ -0,0 +1,28 @@
+
+const options = {year: 'numeric'}
+
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+document.getElementById('lastModified').textContent = document.lastModified;
+
+const visitsDisplay = document.querySelector(".visits");
+const todaysDate = Date.now();
+
+currentdate.textContent = new Date().toLocaleDateString('en-US', options);
+
+const lastVisited = document.getElementById('lastModified');
+lastVisited.textContent = document.lastModified;
+
+let lastVisit = parseInt(window.localStorage.getItem('lastVisit-ls'))  || 0;
+
+if (lastVisit === 0) {
+	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+} else if (lastVisit < 1) {
+	visitsDisplay.textContent = `Back so soon!  Awesome!`;
+}  else {
+    visitsDisplay.textContent = `You last visited ${lastVisit} days ago.`;
+  }
+
+lastVisit++;
+
+localStorage.setItem("lastVisit-ls", lastVisit);
